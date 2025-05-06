2e9069a2445c88762d46684af3456aea5c15b2e6
letigreco <letigreco@hotmail.it>
2025-05-06 16:28:53 +0200

    ppo prova

--- Letizia/train_sb3.py
+++ Letizia/train_sb3.py
@@ -6,6 +6,9 @@
 """
 import gym
 from env.custom_hopper import *
+from stable_baselines3 import PPO, SAC
+from stable_baselines3.common.evaluation import evaluate_policy
+
 
 def main():
     train_env = gym.make('CustomHopper-source-v0')
@@ -17,6 +20,35 @@ def main():
     #
     # TASK 4 & 5: train and test policies on the Hopper env with stable-baselines3
     #
+    model=PPO('MlpPolicy', 
+              train_env,
+              verbose=1,
+              learning_rate=1e-3,
+              n_steps=2048,
+              batch_size=64,
+              n_epochs=20,
+              tensorboard_log="./ppo_hopper_tensorboard/")
+    
+    model.learn(total_timesteps=2e5, tb_log_name="PPO_Hopper")
+    model.save("ppo_hopper")
+    del model  # delete trained model to demonstrate loading
+
+    # Load the saved model
+    model = PPO.load("ppo_hopper")
+
+    # Evaluate the policy
+    mean_reward, std_reward = evaluate_policy(
+        model,
+        train_env,
+        n_eval_episodes=50,
+        deterministic=True,
+        render=True  # True se vuoi visualizzare
+    )
+
+    print(f"Mean reward over 50 episodes: {mean_reward} ± {std_reward}")
+
+
+
 
 if __name__ == '__main__':
     main()
