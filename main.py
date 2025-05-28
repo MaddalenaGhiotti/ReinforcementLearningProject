3de7d2ce51ef159b65766614e5aed61e5895f50a
MaddalenaGhiotti <ghiottimaddalena@gmail.com>
2025-05-28 09:43:45 +0200

    Delete main.py, change n_episodes and add "avg from beginning" graph

--- main.py
+++
@@ -1,3 +0,0 @@
-import module_Reinforce_ActorCritic as rac
-episodes = 5000
-numbers, returns, average_returns, average_beginning, model_name = rac.train(0, hopper='S', n_episodes=episodes, trained_model=None, baseline=0, starting_threshold=700, save_every=75, print_every=episodes//5, plot=True, random_state=42, device='cpu')
