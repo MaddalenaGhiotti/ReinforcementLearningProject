f25f3fd38ce28ffb355145f5013462928da9b15c
MaddalenaGhiotti <ghiottimaddalena@gmail.com>
2025-06-26 19:33:50 +0200

    classes checked and commented

--- AdversarialTraining/classes_beta_batch.py
+++ AdversarialTraining/classes_beta_batch.py
@@ -5,7 +5,8 @@ from torch.nn.utils.rnn import pad_sequence
 from torch.distributions import Normal
 from torch.distributions import Beta
 
-#Funtion to compute G
+
+
 def discount_rewards(r, gamma):
     """
     Computation of return G for each  time-stamp and storage in a tensor
@@ -20,7 +21,9 @@ def discount_rewards(r, gamma):
     return discounted_r
 
 
+
 class Policy(torch.nn.Module):  #Sub-class of NN PyTorch class
+    """ Policy neural network and beta distribution construction"""
     def __init__(self, state_space, action_space, type_alg, layer_size=64):
         super().__init__()
         self.state_space = state_space   #Attribute: state space
@@ -33,12 +36,6 @@ class Policy(torch.nn.Module):  #Sub-class of NN PyTorch class
         self.fc1_actor = torch.nn.Linear(state_space, self.hidden)
         self.fc2_actor = torch.nn.Linear(self.hidden, self.hidden)
         self.fc3_actor = torch.nn.Linear(self.hidden, action_space*2)
-        #self.softplus_actor = torch.nn.Softplus()
-
-        #Learned standard deviation for exploration at training time 
-        #self.beta_activation = F.softplus
-        #init_beta = 0.5
-        #self.beta = torch.nn.Parameter(torch.zeros(self.action_space)+init_beta)  #Treat beta as a parameter to learn like the weights of the NN. A parameter to update, but independent from the state
 
         if type_alg == 2:
             #Critic network
@@ -57,27 +54,12 @@ class Policy(torch.nn.Module):  #Sub-class of NN PyTorch class
 
 
     def forward(self, x):
-        # === DEBUG CHECKS ===
-        #print("fc1_actor weights NaN?:", torch.isnan(self.fc1_actor.weight).any())
-        #print("fc1_actor bias NaN?:", torch.isnan(self.fc1_actor.bias).any())
-    
-        #print("fc1 weight stats:", self.fc1_actor.weight.mean(), self.fc1_actor.weight.std())
-
-        for name, param in self.named_parameters():
-            if torch.isnan(param).any():
-                print(f"{name} contains NaN")
-            #Forward in the actor network
-
+        #Forward in the actor network
         x_actor = self.tanh(self.fc1_actor(x))
-        #print('NN1:',x_actor)
         x_actor = self.tanh(self.fc2_actor(x_actor))
-        #print('NN2:',x_actor)
         action_alpha_beta = F.softplus(self.fc3_actor(x_actor))+1
-        #action_alpha_beta = self.softplus_actor(x_actor)
-        #print('NN output:',action_alpha_beta)
 
-        #beta = self.beta_activation(self.beta)
-        beta_dist = Beta(action_alpha_beta[:3], action_alpha_beta[3:])   #Normalized policy outputs (probabilities of actions)
+        beta_dist = Beta(action_alpha_beta[:3], action_alpha_beta[3:])
 
         if self.type_alg == 2:
             #Forward in the critic network
@@ -89,7 +71,9 @@ class Policy(torch.nn.Module):  #Sub-class of NN PyTorch class
         return beta_dist, (action_alpha_beta[:3].detach(), action_alpha_beta[3:].detach())
 
 
+
 class Value(torch.nn.Module):
+    """ Value neural network for Actor-critic of type 1"""
     def __init__(self, state_space, action_space, layer_size=64):
         super().__init__()
         self.state_space = state_space   #Attribute: state space
@@ -107,7 +91,7 @@ class Value(torch.nn.Module):
     def init_weights(self):
         for m in self.modules():
             if isinstance(m, torch.nn.Linear):
-                torch.nn.init.xavier_uniform_(m.weight)  # Inizializzazione Xavier più stabile
+                torch.nn.init.xavier_uniform_(m.weight)  # Initialization Xavier more stable
                 torch.nn.init.zeros_(m.bias)
 
     def init_weights2(self):
@@ -128,6 +112,7 @@ class Value(torch.nn.Module):
 
 
 class Agent(object):
+    """Agent which learns the policy"""
     def __init__(self, type_alg, policy, value = None, device='cpu', baseline=0, batch_sz=1, gamma=0.99, alpha=0.9, optim_lr=1e-4):
         self.train_device = device
         self.policy = policy.to(self.train_device)
@@ -143,7 +128,7 @@ class Agent(object):
         self.rewards = []
         self.done = []
         self.batch_sz = batch_sz
-
+        #Attributes to track batches
         self.n_episode = 0
         self.probs_list = []
         self.return_list = []
@@ -155,9 +140,7 @@ class Agent(object):
 
 
     def update_policy(self):
-        """
-        From trajectory to optimization step. Update policy params. All time-stamps simultaneously.
-        """
+        """Optimization step of the policy (if necessary)"""
         action_log_probs = torch.stack(self.action_log_probs, dim=0).to(self.train_device).squeeze(-1)   #Concatenate tensors in list along a new axis, move the new tensor on chosen devise and remove entra dimensions of size 1 from the end.
         states = torch.stack(self.states, dim=0).to(self.train_device).squeeze(-1)
         next_states = torch.stack(self.next_states, dim=0).to(self.train_device).squeeze(-1)
@@ -166,16 +149,21 @@ class Agent(object):
 
         self.states, self.next_states, self.action_log_probs, self.rewards, self.done = [], [], [], [], []
         
+        #Algorithm: REINFORCE
         if self.type_alg == 0:
+            #Compute and normalize returns (if needed)
             returns = discount_rewards(rewards, self.gamma)
             #returns -= returns.mean()
             #returns/= (returns.std()+1e-8)
             self.return_list.append(returns)
             self.probs_list.append(action_log_probs)
             self.n_episode +=1
+            #If the current episode is the last of the batch
             if self.n_episode==self.batch_sz:
                 padded_returns = pad_sequence(self.return_list, batch_first=True)
+                #Compute the dynamic baseline as the mean of returns at each timestep
                 baseline = torch.mean(padded_returns, 0)
+                #Compute the total loss of the batch as the sum or average of episode losses
                 tot_loss = 0
                 for i, (action_log_probs, returns) in enumerate(zip(self.probs_list, self.return_list)):
                     loss_fn =-torch.mean(action_log_probs * (returns-baseline[:returns.size(dim=0)]))
@@ -190,7 +178,7 @@ class Agent(object):
                 self.probs_list = []
                 self.return_list = []
                 
-
+        #Algorithm: Actor-Critic of type 1
         elif self.type_alg == 1:
             value_states = self.value(states)
             advantage_term = rewards+self.gamma*self.value(next_states)*(1-done)-value_states
@@ -208,35 +196,28 @@ class Agent(object):
             torch.nn.utils.clip_grad_norm_(self.value.parameters(),1)   #Bring gradient norm to 1 if bigger
             self.optimizer_value.step()   #Compute a step of the optimization algorithm
 
+        #Algorithm: Actor-Critic of type 2
         elif self.type_alg == 2:
-
             # Compute boostrapped discounted return estimates
             _, state_values,_ = self.policy(states)                # V(s_t)
             _, next_state_values,_ = self.policy(next_states)      # V(s_{t+1})
             done = done.float()
-
             td_target = rewards + self.gamma * next_state_values * (1 - done)  # if done=1 → no bootstrapping
-            td_target = td_target.detach()  # Detach from the graph to avoid backpropagation through the next state value
-            
+            td_target = td_target.detach()  # Detach from the graph to avoid backpropagation through the next state value           
             td_error = td_target - state_values  # delta = R_t + gamma*V(s_{t+1}) - V(s_t)
-
             action_log_probs = action_log_probs
             actor_loss = -self.I * action_log_probs * td_error
-            critic_loss = 1/2*(td_error.pow(2))   
-                 
+            critic_loss = 1/2*(td_error.pow(2))                    
             self.optimizer.zero_grad()
             (self.alpha*actor_loss + (1-self.alpha)*critic_loss).backward()
-
             self.optimizer.step()
-
             self.I = self.I * self.gamma  # Update the I for the next step
 
         return 
 
+
     def reset_I(self):
-        """
-        Reset the I value to 1 for the next episode (type_alg == 2)
-        """
+        """Reset the I value to 1 for the next episode (type_alg == 2)"""
         self.I = 1     
         return 
     
@@ -249,6 +230,7 @@ class Agent(object):
         @param evaluation do not act, return distribution mean
         @return action following action in trajectory
         @return action_log_prob probability of chosen action (joint probability of the three action values)
+        @return beta_dist_det detached action beta distribution
         """
         x = torch.from_numpy(state).float().to(self.train_device)
         out,(alpha,beta) = self.policy(x)
@@ -266,15 +248,14 @@ class Agent(object):
             action = beta_dist.sample()
             # Compute Log probability of the action [ log(p(a[0] AND a[1] AND a[2])) = log(p(a[0])*p(a[1])*p(a[2])) = log(p(a[0])) + log(p(a[1])) + log(p(a[2])) ]
             action_log_prob = beta_dist.log_prob(action).sum()
-            return action, action_log_prob, beta_dist_det
+            return action, action_log_prob, beta_dist_det  # beta_dist_det is detached
     
 
     def get_probs(self, state, action):
         """ 
         Computation of action_log_prob given action
         @param state current state
-        @param evaluation do not act, return distribution mean
-        @return action following action in trajectory
+        @param action action of interest
         @return action_log_prob probability of chosen action (joint probability of the three action values)
         """
         x = torch.from_numpy(state).float().to(self.train_device)
@@ -290,9 +271,7 @@ class Agent(object):
 
 
     def store_outcome(self, state, next_state, action_log_prob, reward, done):
-        """
-        Save the step of the trajectory in the class attributes. Store all the trajectory steps together
-        """
+        """Save the step of the trajectory in the class attributes. Store all the trajectory steps together"""
         self.states.append(torch.from_numpy(state).float())
         self.next_states.append(torch.from_numpy(next_state).float())
         self.action_log_probs.append(action_log_prob)
--- Reinforce_ActorCritic/classes.py
+++ Reinforce_ActorCritic/classes.py
@@ -3,7 +3,8 @@ import torch
 import torch.nn.functional as F
 from torch.distributions import Normal
 
-#Funtion to compute G
+
+
 def discount_rewards(r, gamma):
     """
     Computation of return G for each  time-stamp and storage in a tensor
@@ -18,7 +19,9 @@ def discount_rewards(r, gamma):
     return discounted_r
 
 
+
 class Policy(torch.nn.Module):  #Sub-class of NN PyTorch class
+    """ Policy neural network and gaussian distribution construction"""
     def __init__(self, state_space, action_space, type_alg, layer_size=64):
         super().__init__()
         self.state_space = state_space   #Attribute: state space
@@ -72,7 +75,9 @@ class Policy(torch.nn.Module):  #Sub-class of NN PyTorch class
         return normal_dist
 
 
+
 class Value(torch.nn.Module):
+    """ Value neural network for Actor-critic of type 1"""
     def __init__(self, state_space, action_space, layer_size=64):
         super().__init__()
         self.state_space = state_space   #Attribute: state space
@@ -106,6 +111,7 @@ class Value(torch.nn.Module):
 
 
 class Agent(object):
+    """Agent which learns the policy"""
     def __init__(self, type_alg, policy, value = None, device='cpu', baseline=0, gamma=0.99, alpha=0.9, optim_lr=1e-3):
         self.train_device = device
         self.policy = policy.to(self.train_device)
@@ -127,9 +133,7 @@ class Agent(object):
 
 
     def update_policy(self):
-        """
-        From trajectory to optimization step. Update policy params. All time-stamps simultaneously.
-        """
+        """Optimization step of the policy (and value if Actor-Critic)"""
         action_log_probs = torch.stack(self.action_log_probs, dim=0).to(self.train_device).squeeze(-1)   #Concatenate tensors in list along a new axis, move the new tensor on chosen devise and remove entra dimensions of size 1 from the end.
         states = torch.stack(self.states, dim=0).to(self.train_device).squeeze(-1)
         next_states = torch.stack(self.next_states, dim=0).to(self.train_device).squeeze(-1)
@@ -138,10 +142,12 @@ class Agent(object):
 
         self.states, self.next_states, self.action_log_probs, self.rewards, self.done = [], [], [], [], []
         
+        #Algorithm: REINFORCE
         if self.type_alg == 0:
+            #Compute and normalize returns
             returns = discount_rewards(rewards, self.gamma)
             returns -= returns.mean()
-            returns/= (returns.std()+1e-8)  #TODO Normalizzare prima o dopo aver messo la baseline?
+            returns/= (returns.std()+1e-8)
             # Compute loss, gradients and step the optimizer
             loss_fn =-torch.mean(action_log_probs * (returns-self.baseline))
             self.optimizer.zero_grad()
@@ -149,12 +155,11 @@ class Agent(object):
             torch.nn.utils.clip_grad_norm_(self.policy.parameters(),1)   #Bring gradient norm to 1 if bigger
             self.optimizer.step()   #Compute a step of the optimization algorithm
 
+        #Algorithm: Actor-Critic of type 1
         elif self.type_alg == 1:
             value_states = self.value(states)
             advantage_term = rewards+self.gamma*self.value(next_states)*(1-done)-value_states
-
-            actor_loss_fn = -action_log_probs*advantage_term.detach()
-            
+            actor_loss_fn = -action_log_probs*advantage_term.detach()            
             critic_loss_fn = 1/2*(advantage_term.pow(2))
             # ACTOR: compute gradients and step the optimizer
             self.optimizer.zero_grad()
@@ -167,37 +172,29 @@ class Agent(object):
             torch.nn.utils.clip_grad_norm_(self.value.parameters(),1)   #Bring gradient norm to 1 if bigger
             self.optimizer_value.step()   #Compute a step of the optimization algorithm
 
+        #Algorithm: Actor-Critic of type 2
         elif self.type_alg == 2:
-
             # Compute boostrapped discounted return estimates
             _, state_values = self.policy(states)                # V(s_t)
             _, next_state_values = self.policy(next_states)      # V(s_{t+1})
             done = done.float()
-
             td_target = rewards + self.gamma * next_state_values * (1 - done)  # if done=1 → no bootstrapping
-            td_target = td_target.detach()  # Detach from the graph to avoid backpropagation through the next state value
-            
+            td_target = td_target.detach()  # Detach from the graph to avoid backpropagation through the next state value           
             td_error = td_target - state_values  # delta = R_t + gamma*V(s_{t+1}) - V(s_t)
-
             action_log_probs = action_log_probs
             actor_loss = -self.I * action_log_probs * td_error
-            critic_loss = 1/2*(td_error.pow(2))   
-                 
+            critic_loss = 1/2*(td_error.pow(2))                    
             self.optimizer.zero_grad()
             (self.alpha*actor_loss + (1-self.alpha)*critic_loss).backward()
-
             self.optimizer.step()
-
             self.I = self.I * self.gamma  # Update the I for the next step
 
         return 
+    
 
     def reset_I(self):
-        """
-        Reset the I value to 1 for the next episode (type_alg == 2)
-        """
+        """Reset the I value to 1 for the next episode (type_alg == 2)"""
         self.I = 1           
-        
         return 
     
 
@@ -209,6 +206,7 @@ class Agent(object):
         @param evaluation do not act, return distribution mean
         @return action following action in trajectory
         @return action_log_prob probability of chosen action (joint probability of the three action values)
+        @return normal_dist action gaussian distribution
         """
         x = torch.from_numpy(state).float().to(self.train_device)
         out = self.policy(x)
@@ -225,13 +223,13 @@ class Agent(object):
             # Compute Log probability of the action [ log(p(a[0] AND a[1] AND a[2])) = log(p(a[0])*p(a[1])*p(a[2])) = log(p(a[0])) + log(p(a[1])) + log(p(a[2])) ]
             action_log_prob = normal_dist.log_prob(action).sum()
             return action, action_log_prob, normal_dist
-        
+
+
     def get_probs(self, state, action):
         """ 
         Computation of action_log_prob given action
         @param state current state
-        @param evaluation do not act, return distribution mean
-        @return action following action in trajectory
+        @param action action of interest
         @return action_log_prob probability of chosen action (joint probability of the three action values)
         """
         x = torch.from_numpy(state).float().to(self.train_device)
@@ -247,9 +245,7 @@ class Agent(object):
 
 
     def store_outcome(self, state, next_state, action_log_prob, reward, done):
-        """
-        Save the step of the trajectory in the class attributes. Store all the trajectory steps together
-        """
+        """Save the step of the trajectory in the class attributes. Store all the trajectory steps together"""
         self.states.append(torch.from_numpy(state).float())
         self.next_states.append(torch.from_numpy(next_state).float())
         self.action_log_probs.append(action_log_prob)
